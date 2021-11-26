8b6a542c5673571c9e1d7450131620091fb65b09
yarskii <71975048+yarskii@users.noreply.github.com>
2021-11-26 19:00:18 +0300

    Add files via upload

---
+++ lesson_3/common/variables.py
@@ -0,0 +1,19 @@
+DEFAULT_PORT = 7777
+
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+
+MAX_CONNECTIONS = 5
+
+MAX_PACK_LENGTH = 1024
+
+ENCODING = 'utf-8'
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+RESPONDEFAULT_IP_ADDRESSE = 'respondefault_ip_addresse'
